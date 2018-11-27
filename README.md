2fe849f122cb003704c6941716971a1f83770dff
mourajp <moura.joaopedro@gmail.com>
2018-11-27 02:34:16 -0200

    Added @NFLResource Twitter source

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # NFL NEWS for Telegram
 
-This is a Telegram bot that provides the latest news about the NFL powered by [NewsAPI.org](https://newsapi.org).
+This is a Telegram bot that provides the latest news about the NFL powered by [NewsAPI.org](https://newsapi.org) and
+[NFLResearch @ Twitter](https://twitter.com/NFLResearch).
 The bot will fetch news about the NFL every minute from their portal via [NewsAPI](https://newsapi.org) and send to all
 clients that allowed the bot to do so.
 
@@ -13,19 +14,25 @@ information can be translated into fantasy points in this case.
 In order to use this bot you need to create a .ENV file with two specific keys:
 
 * BOT_TOKEN: refers to the bot API token, which one you can get from [@BotFather](https://telegram.me/botfather);
-* NEWS_API_TOKEN: refers to the [NewsAPI.org](https://newsapi.org) API token which one you can get
-[here](https://newsapi.org/princing).
+* NEWS_API_TOKEN: refers to the [NewsAPI.org](https://newsapi.org) API token;
+* TWITTER_CONSUMER_KEY: refers to the key of your [Twitter](https://twitter.com/) client;
+* TWITTER_CONSUMER_SECRET: refers to the secret token of your [Twitter](https://twitter.com/) client.
 
-After set this .ENV file with these two keys, just run the command ```npm install && npm start```. This bot was built on
-top of NodeJS v8.9.4.
+You will need to apply for keys in [NewsAPI.org](https://newsapi.org/princing) and
+[Twitter](https://developer.twitter.com/en/apply-for-access.html). After set this .ENV file with these six keys, just
+run the command ```npm install && npm start```. This bot was built on top of NodeJS v8.9.4.
 
 ## Know Issues
 
 * All chats ids will be lost if the bot resets.
 
+## Disclaimer
+
+If you plan to serve this bot to different users and not just for yourself you gonna probably will need a Privacy Police
 
 ## TODO
 
 * Persist the chat ids;
-* Aggregate tweets from [@NFLResearch](https://twitter.com/NFLResearch) for real time statistics about the games
+* Aggregate posts from [NFL @ Instagram](https://www.instagram.com/nfl/).
+* ~~Aggregate tweets from [@NFLResearch](https://twitter.com/NFLResearch) for real time statistics about the games~~
 * ~~Build a more elegant message, possibly using MarkDown instead using images with caption.~~
--- index.js
+++ index.js
@@ -9,45 +9,101 @@ const mailman = new Telegram(process.env.BOT_TOKEN)
 const NewsAPI = require('newsapi')
 const newsApi = new NewsAPI(process.env.NEWS_API_TOKEN)
 
+const Twit = require('twit')
+const T = new Twit({
+    consumer_key:    process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    app_only_auth:   true
+})
+
 const schedule = require('node-schedule')
 
+const SOURCES = {
+    twitter : "id",
+    instagram: "id",
+    news: "title"
+}
+
 let latestNews = []
+let tweets = []
+let posts = []
 let chats = new Set()
 
+function updateArrayContent(newContents, media) {
+    let updatedContent = []
+    let oldContents = null
+    let send = null
+    let sourceMedia = null
+
+    switch (media) {
+        case "twitter":
+            send = sendTweet
+            sourceMedia = "tweets"
+            oldContents = tweets
+            break
+        case "instagram":
+            send = null
+            sourceMedia = "posts"
+            oldContents = posts
+            break
+        case "news":
+            send = sendNews
+            sourceMedia = "articles"
+            oldContents = latestNews
+            break
+        default:
+            send = null
+            break
+    }
+
+    if(oldContents.length === 0) updatedContent = newContents
+    else {
+        newContents.forEach(newContent => {
+            let isOld = false
+
+            oldContents.forEach(oldContent => {
+                if(oldContent[SOURCES[media]] === newContent[SOURCES[media]]){
+                    isOld = true
+                }
+            })
+
+            if(!isOld) updatedContent.push(newContent)
+        })
+    }
+
+    updatedContent.forEach(content => {
+        chats.forEach(chatID => {
+            send(chatID, content)
+        })
+    })
+
+    if(updatedContent.length === 0) console.log(`Nothing new found on ${media}`)
+    else console.log(`Sent ${updatedContent.length} new ${sourceMedia} to ${chats.size} clients`)
+
+    return newContents
+}
+
 /**
- * Update the database with the latest news
+ * Update the database with the latest news and tweets
  * @param fireDate Date The GMT date that the function was executed
  */
 function updateNews(fireDate) {
+    // Articles from NFL News
     newsApi.v2.topHeadlines({sources: 'nfl-news'})
         .then(response => {
-
-            let newArticles = []
-
-            if(latestNews.length === 0) newArticles = response.articles
-            else {
-                response.articles.forEach(newArticle => {
-                    let isOld = false
-
-                    latestNews.forEach(oldArticle => {
-                        if(oldArticle.title === newArticle.title) isOld = true
-                    })
-
-                    if(!isOld) newArticles.push(newArticle)
-                })
-            }
-
-            newArticles.forEach(article => {
-                chats.forEach(chatId => {
-                    sendNews(chatId, article)
-                })
-            })
-            if(newArticles.length > 0) console.log(`Sent ${newArticles.length} new articles to ${chats.size} chats`)
-            else console.log("No new articles found")
-
-            latestNews = response.articles
+            latestNews = updateArrayContent(response.articles, "news")
         })
-    console.log(`Update news at ${fireDate}`)
+
+    // Tweets from @NFLResearch
+    T.get("statuses/user_timeline",
+        {screen_name: "NFLResearch", count: 10, exclude_replies: true, include_rts: false, tweet_mode: "extended", trim_user: true},
+        (err, data, response) => {
+            if(err) console.log(`Errors: ${err}`)
+            else tweets = updateArrayContent(data, "twitter")
+
+        })
+
+    console.log(`Update contents at ${fireDate}`)
 }
 
 /**
@@ -60,6 +116,26 @@ function sendNews(chatID, article) {
         chatID,
         `[${article.title}. Reported by ${article.author} at ${new Date(article.publishedAt).toString()}](${article.urlToImage})\n\n[${article.description}](${article.url})`,
         {parse_mode: "Markdown"})
+        .catch(err => {
+            console.log(`Error sending article: ${err.message}`)
+        })
+}
+
+/**
+ * Send a tweet content to a specific chat
+ * @param chatId Integer Chat that will receive the tweet
+ * @param tweet Object Tweet object from the Twitter API
+ */
+function sendTweet(chatId, tweet) {
+    let text
+
+    if(tweet.truncated === true) text = tweet.text
+    else text = tweet["full_text"]
+
+    mailman.sendMessage(chatId, text, {parse_mode: "Markdown"})
+        .catch(err => {
+            console.log(`Error sending tweet: ${err.message}`)
+        })
 }
 
 // Update the news every minute
@@ -96,12 +172,16 @@ bot.command("fumble", (ctx) => {
 })
 
 /**
- * Reply the client with the latest news
+ * Reply the client with the latest news and tweet
  */
 bot.command('latest', (ctx) => {
     if(latestNews.length === 0) ctx.reply("Sorry, I don't have the latest news yet 😥")
-    else sendNews(ctx.message.chat.id, latestNews[0])
+    else {
+        sendNews(ctx.message.chat.id, latestNews[0])
+        sendTweet(ctx.message.chat.id, tweets[0])
+    }
 })
+
 
 // Easter eggs
 bot.hears('Da Bears', (ctx) => ctx.reply('GO BEARS! 🐻⬇️'))

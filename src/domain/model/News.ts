d788f34e0477929a3a5aea6693fac19e1e176ddd
João Pedro Santos de Moura <moura.joaopedro@gmail.com>
2020-12-06 20:21:47 -0300

    refactor(code-smell-fix): news hash code is now calculated in a more apropriated moment

--- src/domain/model/News.ts
+++ src/domain/model/News.ts
@@ -8,14 +8,13 @@ export default class News {
 
   source: NewsSource;
 
+  hashCode: string;
+
   constructor(headline: string, body: string, source: NewsSource) {
     this.headline = headline;
     this.body = body;
     this.source = source;
-  }
-
-  get hashCode(): string {
-    return crypto.createHash('sha256').update(this.body).digest('hex');
+    this.hashCode = crypto.createHash('sha256').update(this.body).digest('hex');
   }
 
   toString(): string {
